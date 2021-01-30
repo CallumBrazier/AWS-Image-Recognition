f62eba33cc0f4529d1a5fad589ccbcbe58829a9f
Callum Brazier <braziercallum@gmail.com>
2021-01-30 17:50:31 +1300

    adding in aws rekognition image analysis for url uploaded images

--- upload.js
+++ upload.js
@@ -3,9 +3,9 @@ const upload = require("express-fileupload");
 const port = 5000;
 const fs = require("fs");
 const https = require("https");
+const request = require("request");
 require("dotenv").config();
 var AWS = require("aws-sdk");
-// var uuid = require("node-uuid");
 
 const config = new AWS.Config({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -26,6 +26,7 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+//save image from desktop to local disk and send it to AWS Rekognition to analyse.
 app.post("/", (req, res) => {
   if (req.files) {
     console.log("files", req.files);
@@ -42,28 +43,41 @@ app.post("/", (req, res) => {
       } else {
         let bitmap = req.files.filename.data;
         AnalysePhoto(bitmap);
+        res.send("File Uploaded and analysed!");
       }
     });
   }
 });
 
+//save image from URL to local disk and send it to AWS Rekognition to analyse.
 app.post("/url", (req, res) => {
   if (req.body.url) {
     let url = req.body.url;
     console.log(url);
+
+    let options = {
+      url: url,
+      method: "get",
+      encoding: null,
+    };
+
     const saveImage = (url, path) => {
       let localPath = fs.createWriteStream(path);
       let request = https.get(url, function (response) {
         response.pipe(localPath);
         console.log("File uploaded!");
-        res.send("File uploaded!");
+        res.send("File uploaded and analysed!");
       });
     };
 
-    const bitmap = fs.readFileSync("./uploads/1611909057698.jpg");
-    console.log("bitmap", bitmap);
+    request(options, function (err, res, body) {
+      if (err) {
+        console.error("error: ", err);
+      } else {
+        AnalysePhoto(body);
+      }
+    });
     saveImage(url, "./uploads/" + Date.now() + ".jpg");
-    AnalysePhoto(bitmap);
   }
 });
 
